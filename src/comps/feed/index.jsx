47933d95fe4ea13bca63ad8b7f136a4cc05b2f9f
ssn00zee <cjavillo@my.bcit.ca>
2023-04-08 16:51:51 -0700

    new styles add

--- src/comps/feed/index.jsx
+++ src/comps/feed/index.jsx
@@ -5,22 +5,24 @@ export default function Feed({
   // console.log(posts)
 
   return (
-    <div className="w-3/4 flex justify-center flex-col gap-6">
-      <h1>Feed</h1>
-      {posts &&
-        posts.map((post) => {
-          
-          const { id, title, content } = post
+    <div className="w-[80%] flex justify-center flex-col gap-6 m-4">
+      <h1 className="text-4xl">Feed</h1>
+      <div className="border p-4 flex flex-col gap-4">
+        {posts &&
+          posts.map((post) => {
+            
+            const { id, title, content } = post
 
 
-          return (
-            <div key={id} className='flex flex-col gap-4 w-64'>
-              <h1>{title}</h1>
-              <p>{content}</p>
-            </div>
-          );
-        })
-      }
+            return (
+              <div key={id} className='flex flex-col gap-4 w-64 border p-4'>
+                <h1 className="text-3xl">{title}</h1>
+                <p>{content}</p>
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
   )
 }
