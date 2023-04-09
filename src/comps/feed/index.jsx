22934bcb39ba320156044da45bbe2831c789878c
ssn00zee <cjavillo@my.bcit.ca>
2023-04-08 17:12:21 -0700

    added time

--- src/comps/feed/index.jsx
+++ src/comps/feed/index.jsx
@@ -2,7 +2,7 @@ export default function Feed({
   posts
 }){
 
-  // console.log(posts)
+  console.log(posts)
 
   return (
     <div className="w-[80%] flex justify-center flex-col gap-6 m-4">
@@ -11,15 +11,24 @@ export default function Feed({
         {posts &&
           posts.map((post) => {
             
-            const { id, title, content } = post
+            const { id, title, content, createdAt } = post
 
+            const time = () => {
+              const utcTimestamp = createdAt
+              const date = new Date(utcTimestamp)
+              const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' }
+              const dateString = date.toLocaleDateString('en-US', options)
+
+              return dateString
+            }
 
             return (
               <div key={id} className='flex flex-col gap-4 w-64 border p-4'>
                 <h1 className="text-3xl">{title}</h1>
                 <p>{content}</p>
+                <p>{time()}</p>
               </div>
-            );
+            )
           })
         }
       </div>
